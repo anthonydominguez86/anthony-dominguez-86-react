c41d4e8b8a4aa271d0e97cd8cbe6d0b18c44e42c
anthonydominguez86 <adominguez11186@gmail.com>
2021-05-05 10:03:46 -0400

    added pages folder with portfolio pages

---
+++ portfolio/src/components/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function About() {
+    return(
+    <div>
+    <h1>About Me</h1> 
+    <hr></hr>        
+    <img src="./assets/adominguez.jpg" class="rounded float-start img-fluid" alt="adominguez.jpg" style="padding:10px" width= "150" height="200"></img>
+        <p>
+          I started my career as a chef but I am now changing that career to be a full stack web developer. 
+          About six months ago I became very interested in the field and started doing some extensive research 
+          I came across the Georgia Tech Boot Camp and the journey began.  So far I have been immersed in so much new material it almost mind-blowing. 
+          I first learned of the simple and plain HTML then how it is styled with CSS and now we are onto the much more complex node.JS, express, MySQL, and MongoDB. 
+          I have enjoyed learning this field so far and will enjoy everything I am going to learn. It will be a long road but I am going to take from it as much as I can and put into it all I have. 
+          I believe this will make me a great web developer and I am anxious to be able to add to this portfolio and make it a showcase for my newly acquired talent and for my new career. 
+          Please enjoy my work with professional links below as well as some screenshots of my work in the portfolio link above. 
+          <hr></hr>
+          <a class="nav-link" href="https://github.com/anthonydominguez86" target="Git Hub">Git Hub</a>
+          <a class="nav-link" href="https://www.linkedin.com/in/anthony-dominguez-4884529b/" target="Linked In">Linked In</a>
+          <a class= "nav-link" href="./assets/Resume Anthony Dominguez.docx" download>Resume</a>
+          <hr></hr>
+          <br></br>
+        </p> 
+    </div>
+);
+}
+
+export default About;
