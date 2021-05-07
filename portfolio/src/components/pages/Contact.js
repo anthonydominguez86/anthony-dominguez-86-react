1c71dbdd4a896319ad6a12b7d8f68a1d792f1a02
anthonydominguez86 <adominguez11186@gmail.com>
2021-05-07 11:15:00 -0400

    added pages for portfolio and self image to public folder

---
+++ portfolio/src/components/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Contact() {
+    return(
+<div>
+<h1>Contact</h1>
+    <hr></hr>
+    <p>Email: adominguez11186@gmail.com</p>
+    <p>Contact: (305) 336 7224</p>
+    <hr></hr>
+      <a class="nav-link" href="https://github.com/anthonydominguez86" target="Git Hub">Git Hub</a>
+      <a class="nav-link" href="https://www.linkedin.com/in/anthony-dominguez-4884529b/" target="Linked In">Linked In</a>
+      <a class= "nav-link" href="./assets/Resume Anthony Dominguez.docx" download>Resume</a>
+    <hr></hr>
+  </div>
+  );
+}
+
+export default Contact;
---
+++ portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Portfolio() {
+    return(
+    <div>
+    <aside>
+        <h1>Portfolio</h1>
+        <hr></hr>
+        <h5>Click the images below to check out the website and GitHub repository</h5>
+        <hr></hr>
+        <p>Ment2Trade</p>
+        <a href= "https://rhubarb-cake-73090.herokuapp.com/"><img src="./assets/screenshot8.png" class="rounded float-left" alt="project" style={{margin: "20px"}} height="170" width="250"></img></a> 
+        <a href= "https://github.com/Milan1422/A-Brave-Dolphin-App"><img src= "./assets/screenshot10.png" class="rounded float-right" alt="project2" style={{margin: "20px"}} height="170" width="250"></img></a>
+        <hr></hr>
+        <p>The Practical Exchange</p>
+        <a href= "https://milan1422.github.io/The-Practical-Exchange/"><img src= "./assets/Practical Exchange3.png" class="rounded float-left" alt="project3" style={{margin: "20px"}} height="170" width="250"></img></a>
+        <a href= "https://github.com/Milan1422/The-Practical-Exchange"><img src= "./assets/Practical Exchange5.png" class="rounded float-right" alt="project4" style={{margin: "20px"}} height="170" width="250"></img></a>
+        <hr></hr>
+        <p>Password Generator</p>
+        <a href= "https://anthonydominguez86.github.io/weather-dashboard-week-6/"><img src= "./assets/weather dash 3.png" class="rounded float-left" alt="weather" style={{margin: "20px"}} height="170" width="250"></img></a> 
+        <a href= "https://github.com/anthonydominguez86/weather-dashboard-week-6"><img src= "./assets/weather dash 4.png" class="rounded float-right" alt="weather2" style={{margin: "20px"}} height="170" width="250"></img></a>    
+    </aside>
+    </div>
+
+    );
+}
+
+export default Portfolio;
