07bd909c6eb4e246d63a55c4dde777b40afa821e
anthonydominguez86 <adominguez11186@gmail.com>
2021-08-17 20:28:19 -0400

    adding icons for the links and resume

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -9,11 +9,11 @@ function Contact() {
     <p>Email: adominguez11186@gmail.com</p>
     <p>Contact: (305) 336 7224</p>
     <hr></hr>
-      <a class="link" href="https://github.com/anthonydominguez86" target="Git Hub">Git Hub</a>
-      <br></br>
-      <a class="link" href="https://www.linkedin.com/in/anthony-dominguez-4884529b/" target="Linked In">Linked In</a>
-      <br></br>
-      <a class= "link" href="./assets/Resume Anthony Dominguez.docx" download>Resume</a>
+    <a class="link" href="https://github.com/anthonydominguez86" target="Git Hub"><i class="fab fa-github-square fa-3x"></i> Git Hub</a>
+    <br></br>
+    <a class="link" href="https://www.linkedin.com/in/anthony-dominguez-4884529b/" target="Linked In"><i class="fab fa-linkedin fa-3x"></i> Linked In</a>
+    <br></br>
+    <a class= "link" href="./assets/Resume Anthony Dominguez.docx" download="Resume"><i class="fas fa-envelope-open-text fa-3x"></i> Resume</a>
     <hr></hr>
     </form>
   </div>
