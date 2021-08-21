afa934dd5fd788f87b690e3481c3e4bfb5e13826
anthonydominguez86 <adominguez11186@gmail.com>
2021-08-20 20:24:56 -0400

    routing issues for page loading fixed opens at landing page (aboutpage for now)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Landing from "./components/pages/Landing";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Portfolio from "./components/pages/Portfolio";
@@ -11,7 +12,7 @@ function App() {
     <Router>
       <div>
         <NavBar />
-        <Route path="/" exact component={About} />
+        <Route path="/anthony-dominguez-86-react" exact component={Landing} />
         <Route path="/about" exact component={About} />
         <Route path="/contact" exact component={Contact} />
         <Route path="/portfolio" exact component={Portfolio} />
