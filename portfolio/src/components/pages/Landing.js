826aa6cf44b8037e6d624e24d6c7e3e6d13e1db7
anthonydominguez86 <adominguez11186@gmail.com>
2021-08-24 21:13:21 -0400

    added code for carousel and imported it from bootstrap

--- portfolio/src/components/pages/Landing.js
+++ portfolio/src/components/pages/Landing.js
@@ -2,6 +2,8 @@ import React from "react";
 import Image from "../assets/adominguez.jpg"
 import Image2 from "../assets/screenshot8.png"
 import Image3 from "../assets/employee directory.png"
+import Image4 from "../assets/weather dash 4.png"
+import Carousel from 'react-bootstrap/Carousel'
 
 function Landing() {
     return(
@@ -9,8 +11,7 @@ function Landing() {
     <section>
     <h1>About Me</h1> 
     <hr></hr>        
-    <img src={Image} class="rounded float-start img-fluid" alt="adominguez.jpg" style={{padding:"10px"}} width= "150" height="200"></img>
-    <hr></hr>        
+    <img src={Image} class="rounded float-start img-fluid" alt="adominguez.jpg" style={{padding:"10px"}} width= "150" height="200"></img>       
         <p>
           I started my career as a chef but I am now changing that career to be a full stack web developer. 
           I became very interested in the field and started doing some extensive research 
@@ -29,44 +30,43 @@ function Landing() {
           <hr></hr>
           <br></br>
         </p> 
-        
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel"></div>
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner"></div>
-    <div class="carousel-item active"></div>
-      <img src={Image2} class="d-block w-100" alt="Ment2Trade"></img>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Ment2Trade</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    <div></div>
-    <div class="carousel-item"></div>
-      <img src={Image3} class="d-block w-100" alt="..."></img>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-      <div></div>
-    <div class="carousel-item"></div>
-      <img src="..." class="d-block w-100" alt="..."></img>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-      <div></div>
-      <div></div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+  <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Image2}
+      alt="Ment2Trade"
+    />
+    <Carousel.Caption>
+      <h3>Ment2Trade</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Image3}
+      alt="Employee Directory"
+    />
+
+    <Carousel.Caption>
+      <h3>Employee Directory</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Image4}
+      alt="Weather Dash"
+    />
+
+    <Carousel.Caption>
+      <h3>Local Weather Dashboard</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
   </section>
   </div>
 );
