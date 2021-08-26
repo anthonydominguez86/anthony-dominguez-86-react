5a9ee5e54cb1818f0e445ad1e6c03b8d58391d8f
anthonydominguez86 <adominguez11186@gmail.com>
2021-08-25 21:11:00 -0400

    hamburger menu added

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -33,9 +33,9 @@ function Navbar(){
         >
           Portfolio
         </Link>
-          {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button> */}
+          </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="nav nav-bar"> 
               <li class="nav-item">
