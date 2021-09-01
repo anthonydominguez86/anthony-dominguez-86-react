e6828b6cc8ce2bdbe76a773a03498e61ab8fab18
anthonydominguez86 <adominguez11186@gmail.com>
2021-08-31 20:25:40 -0400

    added breaks in page to show each project with title and langauges

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -26,27 +26,28 @@ function Portfolio() {
         <p>MySQL | JQuery| Bootstrap | HandlebarsJS</p>
         <a href= "https://rhubarb-cake-73090.herokuapp.com/"><img src={Image} class="rounded float-left" alt="project" style={{margin: "20px"}} height="170" width="250"></img></a> 
         <a href= "https://github.com/Milan1422/A-Brave-Dolphin-App"><img src= {Image1} class="rounded float-right" alt="project2" style={{margin: "20px"}} height="170" width="250"></img></a>
-
+        <hr></hr>
         <h3>The Practical Exchange</h3> 
         <p>HTML5 | CSS3 | Bulma | JS </p>
         <a href= "https://milan1422.github.io/The-Practical-Exchange/"><img src= {Image2} class="rounded float-left" alt="project3" style={{margin: "20px"}} height="170" width="250"></img></a>
         <a href= "https://github.com/Milan1422/The-Practical-Exchange"><img src= {Image3} class="rounded float-right" alt="project4" style={{margin: "20px"}} height="170" width="250"></img></a>
-
-        <h3>Employee Directory</h3> 
+        <hr></hr>
+        <h3>Employee Directory</h3>
         <p>HTML5 | CSS3 | React | JS </p>
+        {/* <p>An employee directory using filters and search function using React.</p>  */}
         <a href= "hhttps://anthonydominguez86.github.io/react-employee-directory/"><img src= {Image4} class="rounded float-left" alt="project3" style={{margin: "20px"}} height="170" width="250"></img></a>
         <a href= "https://github.com/anthonydominguez86/react-employee-directory"><img src= {Image5} class="rounded float-right" alt="project4" style={{margin: "20px"}} height="170" width="250"></img></a>
-
+        <hr></hr>
         <h3>Note Taker</h3> 
         <p>HTML5 | CSS3 | Express | Routing | JS </p>
         <a href= "https://guarded-bastion-41842.herokuapp.com/"><img src= {Image6} class="rounded float-left" alt="project3" style={{margin: "20px"}} height="170" width="250"></img></a>
         <a href= "https://github.com/anthonydominguez86/note-taker-express"><img src= {Image7} class="rounded float-right" alt="project4" style={{margin: "20px"}} height="170" width="250"></img></a>
-
+        <hr></hr>
         <h3>E-commerce Backend</h3>
         <p>Express | Node  | MySQL | JSON | Postman </p>
         <a href= "https://drive.google.com/file/d/1T-Jn6DlHpZge_yvpi0eaUClufl8Ats7i/view?usp=sharing"><img src= {Image8} class="rounded float-left" alt="project3" style={{margin: "20px"}} height="170" width="250"></img></a>
         <a href= "https://github.com/anthonydominguez86/e-commerce-backend"><img src= {Image9} class="rounded float-right" alt="project4" style={{margin: "20px"}} height="170" width="250"></img></a>
-        
+        <hr></hr>
         <h3>Local Weather Dashboard</h3>
         <p>HTML5 | CSS3 | Bootstrap | API | JS </p>
         <a href= "https://anthonydominguez86.github.io/weather-dashboard-week-6/"><img src= {Image10} class="rounded float-left" alt="weather" style={{margin: "20px"}} height="170" width="250"></img></a> 
