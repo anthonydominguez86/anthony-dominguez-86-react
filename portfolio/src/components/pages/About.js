b5b57f8676d8d543a563e53ae1e3daf494a12916
anthonydominguez86 <adominguez11186@gmail.com>
2021-09-24 08:25:31 -0400

    reworded content on about and landing page

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -13,7 +13,7 @@ function About() {
           I started my career as a chef but I am now changing that career to be a full stack web developer. 
           I became very interested in the field and started doing some extensive research 
           I came across the Georgia Tech Boot Camp and the journey began.  So far I have been immersed in so much new material it almost mind-blowing. 
-          I first learned of the simple and plain HTML then how it is styled with CSS and now we are onto the much more complex node.JS, express, MySQL, MongoDB and React. 
+          I first learned of the basics HTML then how it is styled with CSS and now we are onto the much more complex node.JS, express, SQL, noSQL, MongoDB and React. 
           I have enjoyed learning this field so far and will enjoy everything I am going to learn. It will be a long road but I am going to take from it as much as I can and put into it all I have. 
           I believe this will make me a great web developer and I am anxious to be able to add to this portfolio and make it a showcase for my newly acquired talent and for my new career. 
           Please enjoy my work with professional links below, as well as some screenshots of my work in the portfolio link above each screenshot leads to the deployed website and 
